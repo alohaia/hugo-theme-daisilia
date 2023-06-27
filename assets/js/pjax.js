2eb947d52e93e1eeff6206fd01227549d9022df0
alohaia <liu.qihuan@outlook.com>
2023-06-27 19:39:49 +0800

    fix: hide hover summary while changing page

--- assets/js/pjax.js
+++ assets/js/pjax.js
@@ -23,6 +23,9 @@ document.addEventListener("pjax:send", function () {
     document.querySelectorAll('.heading').forEach((heading) => {
         TOCOnscrollObserver.unobserve(heading);
     });
+
+    summaryContentEl.classList.add("hide");
+    console.log("hide")
 });
 
 // Pjax 完成之后执行的函数
