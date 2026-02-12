17297371532ed1412a47e6f73cf83a4e33df9654
alohaia <liu.qihuan@outlook.com>
2026-02-12 22:43:38 +0800

    fix: generate images of rmarkdown files correctly

--- utils/R/build_one.R
+++ utils/R/build_one.R
@@ -3,16 +3,39 @@
 
 local({
     # input/output filenames are passed as two additional arguments to Rscript
-    a <- commandArgs(TRUE)
+    a <- commandArgs(trailingOnly = TRUE)
     reld <- gsub("^content/|(/_?index)?\\.Rmd$", "", a[1])
 
+    named_args <- a[grepl(pattern = "^--\\w+=\\S+", a)]
+    a <- a[!(a %in% named_args)]
+
+    parsed <- sapply(named_args, function(x) {
+        strsplit(sub("^--", "", x), "=")
+    })
+    parsed <- setNames(
+        lapply(parsed, function(x) x[2]),
+        lapply(parsed, function(x) x[1])
+    )
+
+    prefix <- ""
+    if ("prefix" %in% names(parsed)) {
+        prefix <- parsed$prefix    } else {
+        prefix <- rprojroot::find_root(
+            rprojroot::has_dir(".git") | rprojroot::has_file_pattern("README.*")
+        )
+    }
+
+    a[1] <- file.path(prefix, a[1])
+    a[2] <- file.path(prefix, a[2])
+    target_dir <- file.path(prefix, "static/R-figures/", reld, "")
+
     # clean up old figures
-    unlink(sprintf("../static/R-figures/%s/", reld), recursive = FALSE)
+    unlink(target_dir, recursive = TRUE)
+
+    print(c(a, target_dir))
 
     knitr::opts_chunk$set(
-        fig.path   = sprintf("../static/R-figures/%s/", reld),
-        cache      = TRUE,
-        cache.path = sprintf("../.cache/%s/", reld),
+        fig.path   = target_dir,
         comment    = "#>",
         results    = "hold",
         tiry       = TRUE,
@@ -26,8 +49,8 @@ local({
         unnamed.chunk.label = ""
     )
     options(digits = 4)
-    knitr::knit(a[1], a[2],
-        quiet = TRUE, encoding = "UTF-8", envir = .GlobalEnv
+    knitr::knit(
+        a[1], a[2], quiet = TRUE, encoding = "UTF-8", envir = .GlobalEnv
     )
 
     text <- readLines(a[2], encoding = "UTF-8")
